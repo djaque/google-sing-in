68a30f2dd42599146d620ae4db862fbf9044bbea
Dany Jaque <djaque@gmail.com>
2020-03-12 09:17:06 -0300

    First Version for google-sing-in

---
+++ go.mod
@@ -0,0 +1,11 @@
+module google-sing-in
+
+go 1.13
+
+require (
+	github.com/boj/redistore v0.0.0-20180917114910-cd5dcc76aeff // indirect
+	github.com/gin-gonic/contrib v0.0.0-20191209060500-d6e26eeaa607
+	github.com/gin-gonic/gin v1.5.0
+	github.com/gorilla/sessions v1.2.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+)
---
+++ google-sing-in.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/contrib/sessions"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+// Credentials which stores google ids.
+type Credentials struct {
+	Cid     string `json:"client_id"`
+	Csecret string `json:"client_secret"`
+}
+
+// User is a retrieved and authentiacted user.
+type User struct {
+	Sub           string `json:"sub"`
+	Name          string `json:"name"`
+	GivenName     string `json:"given_name"`
+	FamilyName    string `json:"family_name"`
+	Profile       string `json:"profile"`
+	Picture       string `json:"picture"`
+	Email         string `json:"email"`
+	EmailVerified bool   `json:"email_verified"`
+	Gender        string `json:"gender"`
+}
+
+var cred Credentials
+var conf *oauth2.Config
+var state string
+var store = sessions.NewCookieStore([]byte("secret"))
+
+func randToken() string {
+	b := make([]byte, 32)
+	rand.Read(b)
+	return base64.StdEncoding.EncodeToString(b)
+}
+
+func init() {
+	file, err := ioutil.ReadFile("./creds.json")
+	if err != nil {
+		log.Printf("File error: %v\n", err)
+		os.Exit(1)
+	}
+	log.Printf("File: %+v\n", string(file))
+	if err := json.Unmarshal(file, &cred); err != nil {
+		panic(err)
+	}
+	log.Printf("Json: %+v\n", cred)
+
+	conf = &oauth2.Config{
+		ClientID:     cred.Cid,
+		ClientSecret: cred.Csecret,
+		RedirectURL:  "http://regress.yapo.cl:9090/auth",
+		Scopes: []string{
+			"profile",
+			// You have to select your own scope from here -> https://developers.google.com/identity/protocols/googlescopes#google_sign-in
+		},
+		Endpoint: google.Endpoint,
+	}
+}
+
+func indexHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.tmpl", gin.H{
+		"title": "Login con Google",
+	})
+}
+
+func getLoginURL(state string) string {
+	return conf.AuthCodeURL(state)
+}
+
+func authHandler(c *gin.Context) {
+	// Handle the exchange code to initiate a transport.
+	session := sessions.Default(c)
+	retrievedState := session.Get("state")
+	if retrievedState != c.Query("state") {
+		c.AbortWithError(http.StatusUnauthorized, fmt.Errorf("Invalid session state: %s", retrievedState))
+		return
+	}
+
+	tok, err := conf.Exchange(oauth2.NoContext, c.Query("code"))
+	if err != nil {
+		c.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	client := conf.Client(oauth2.NoContext, tok)
+	email, err := client.Get("https://www.googleapis.com/oauth2/v3/userinfo")
+	if err != nil {
+		c.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+	defer email.Body.Close()
+	data, _ := ioutil.ReadAll(email.Body)
+	log.Printf("BODY:%s\n", string(data))
+	user := User{}
+	if err := json.Unmarshal(data, &user); err != nil {
+		panic(err)
+	}
+
+	log.Printf("Email body:  %+v\n", user)
+	c.HTML(http.StatusOK, "success.html", gin.H{
+		"title":   "Logged with Google",
+		"email":   user.Email,
+		"picture": user.Picture,
+	})
+}
+
+func loginHandler(c *gin.Context) {
+	state = randToken()
+	session := sessions.Default(c)
+	session.Set("state", state)
+	session.Save()
+	c.Writer.Write([]byte("<html><title>Golang Google</title> <body> <a href='" + getLoginURL(state) + "'><button>Login with Google!</button> </a> </body></html>"))
+}
+
+func main() {
+	router := gin.Default()
+	router.Use(sessions.Sessions("goquestsession", store))
+	router.Static("/css", "./static/css")
+	router.Static("/img", "./static/img")
+	router.LoadHTMLGlob("templates/*")
+
+	router.GET("/", indexHandler)
+	router.GET("/login", loginHandler)
+	router.GET("/auth", authHandler)
+
+	router.Run("127.0.0.1:9090")
+}
